c5e9aed733308f4e49cabd766b2c66024733ecc4
CoderTofu <vioneverforget@gmail.com>
2024-05-23 10:36:29 +0800

    file directory update

--- src/pages/Over.js
+++ src/pages/Over.js
@@ -5,12 +5,12 @@ export default function Over({imageData}) {
     console.log(imageData)
 
     return (
-        <>
+        <div>
             {imageData.map((element, index) => {
                 return (
                     <img key={index} src={element.URL} alt={element.emoji} />
                 )
             })}
-        </>
+        </div>
     )
 }
