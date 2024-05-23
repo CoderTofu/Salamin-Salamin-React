a3b19b8b841cc0846b06e34f0ff12169de58bf29
CoderTofu <vioneverforget@gmail.com>
2024-05-23 10:43:27 +0800

    directory update again with css for over page

--- src/pages/Over.js
+++ src/pages/Over.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useRef, } from 'react';
+import '../css/over.css'
 
 export default function Over({imageData}) {
 
     console.log(imageData)
 
     return (
-        <div>
+        <div className='image-container'>
             {imageData.map((element, index) => {
                 return (
                     <img key={index} src={element.URL} alt={element.emoji} />
